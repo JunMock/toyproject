9df85d8dbeb20ab1732c373cc637daa9995f35a7
JunMock <kjmzzang01@gmail.com>
2021-03-15 18:38:01 +0900

    add vuex and add comment for dependencies

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # toyproject
+
+## back
+
+- spring boot 2.4.3
+- java 11
+- spring boot devtoiols
+- rombok
+- spring web
+- spring data jpa
+
+## front
+
+- vue
+- vuex
+- vue-router
+- axios
+- quasar
--- front/src/quasar.js
+++ front/src/quasar.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 import './styles/quasar.scss'
-import lang from 'quasar/lang/(ko-kr).js'
+import lang from 'quasar/lang/ko-kr.js'
 import '@quasar/extras/material-icons/material-icons.css'
 import { Quasar } from 'quasar'
 
