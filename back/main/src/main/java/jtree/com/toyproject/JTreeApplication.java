a1986d6adc2970b03886f9c79d4c185a892243de
JunMock <kjmzzang01@gmail.com>
2021-03-15 18:29:55 +0900

    create prj and setting default

---
+++ back/main/src/main/java/jtree/com/toyproject/JTreeApplication.java
@@ -0,0 +1,13 @@
+package jtree.com.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JTreeApplication.class, args);
+	}
+
+}
---
+++ back/main/src/test/java/jtree/com/toyproject/JTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package jtree.com.toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
